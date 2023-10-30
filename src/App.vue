c7b68f16ed088a4ddb9d669721c2639c7957495d
Tyler <yumenohik@gmail.com>
2023-10-30 15:43:45 +0900

    It's Just an AJAX Request

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,59 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" :posts="posts"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import {onMounted, ref} from "vue";
+
+
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  setup() {
+    const posts = ref([])
+
+    onMounted(() => {
+      fetch('http://lc-laravel-graphql.test/graphql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          query: `
+        query {
+          posts {
+            data {
+              id
+              title
+            }
+          }
+        }
+      `
+        })
+
+      })
+          .then(res => res.json())
+          .then(result => {
+            console.log(result)
+            posts.value = result.data.posts.data
+          })
+    });
+
+    return {
+      posts
+    }
   }
 }
+
+
+
+
+
 </script>
 
 <style>
